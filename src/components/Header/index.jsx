0c40274c03b63af4ecc410e3f9c504b536cbcc86
NdellaSn <ndellasene3@gmail.com>
2024-01-25 14:25:25 +0100

    headerv1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Header.scss";
+import logo from "../../assets/images/logo.png";
 
 function Header() {
   return (
     <header>
-      <nav>ici header</nav>
+      <Link to="/">
+        <img src={logo} alt="logo de kasa" />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/a propos">A Propos</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
